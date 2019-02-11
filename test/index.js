16fa1da97e87e9292e27ac7bce801eea45469d9b
Radu Luncasu <radu.luncasu@gmail.com>
2019-02-11 16:47:41 +0200

    clearer test for referrer

--- test/index.js
+++ test/index.js
@@ -19,10 +19,10 @@ it('should create a DI xml object to post (using visitorID)', function () {
         eVar10: 'test evar10 value',
         prop10: 'test prop10 value',
         events: "event10,event11",
-        referrer: adobeAnalyticsHelper.getReportingSuiteId()
+        referrer: "https://dummy.referrer.com"
     };
 
-    var testResult = '<visitorID>myvisitorId</visitorID><pageName>My Home Page</pageName><channel>My Channel name</channel><eVar10>test evar10 value</eVar10><prop10>test prop10 value</prop10><events>event10,event11</events><referrer>MY-REPORTING-SUITE-ID</referrer><reportSuiteID>MY-REPORTING-SUITE-ID</reportSuiteID>';
+    var testResult = '<visitorID>myvisitorId</visitorID><pageName>My Home Page</pageName><channel>My Channel name</channel><eVar10>test evar10 value</eVar10><prop10>test prop10 value</prop10><events>event10,event11</events><referrer>https://dummy.referrer.com</referrer><reportSuiteID>MY-REPORTING-SUITE-ID</reportSuiteID>';
 
     var myDi = adobeAnalyticsHelper.getDataInsertion(callData);
 
@@ -37,10 +37,10 @@ it('should create a DI xml object to post (using ipaddress)', function () {
         eVar10: 'test evar10 value',
         prop10: 'test prop10 value',
         events: "event10,event11",
-        referrer: adobeAnalyticsHelper.getReportingSuiteId()
+        referrer: "https://dummy.referrer.com"
     };
 
-    var testResult = '<ipaddress>127.0.0.1</ipaddress><pageName>My Home Page</pageName><channel>My Channel name</channel><eVar10>test evar10 value</eVar10><prop10>test prop10 value</prop10><events>event10,event11</events><referrer>MY-REPORTING-SUITE-ID</referrer><reportSuiteID>MY-REPORTING-SUITE-ID</reportSuiteID>';
+    var testResult = '<ipaddress>127.0.0.1</ipaddress><pageName>My Home Page</pageName><channel>My Channel name</channel><eVar10>test evar10 value</eVar10><prop10>test prop10 value</prop10><events>event10,event11</events><referrer>https://dummy.referrer.com</referrer><reportSuiteID>MY-REPORTING-SUITE-ID</reportSuiteID>';
 
     var myDi = adobeAnalyticsHelper.getDataInsertion(callData);
 
