f463eaaffa294004f4b1628f4d6c4bf65306458d
Zoran Nikolovski <nikolovs@adobe.com>
2019-11-28 14:55:03 +1100

    adding tests for new imsregion property

--- test/index.js
+++ test/index.js
@@ -46,3 +46,22 @@ it('should create a DI xml object to post (using ipaddress)', function () {
 
     myDi.getPostXmlRequestBody().should.equal(testResult);
 });
+
+it('should create a DI xml object to post and SSF (using imsregion)', function () {
+    var callData = {
+        ipaddress: '127.0.0.1',
+        imsregion: '8',
+        pageName: 'My Home Page',
+        channel: 'My Channel name',
+        eVar10: 'test evar10 value',
+        prop10: 'test prop10 value',
+        events: "event10,event11",
+        referrer: "https://dummy.referrer.com"
+    };
+
+    var testResult = '<ipaddress>127.0.0.1</ipaddress><imsregion>8</imsregion><pageName>My Home Page</pageName><channel>My Channel name</channel><eVar10>test evar10 value</eVar10><prop10>test prop10 value</prop10><events>event10,event11</events><referrer>https://dummy.referrer.com</referrer><reportSuiteID>MY-REPORTING-SUITE-ID</reportSuiteID>';
+
+    var myDi = adobeAnalyticsHelper.getDataInsertion(callData);
+
+    myDi.getPostXmlRequestBody().should.equal(testResult);
+});
